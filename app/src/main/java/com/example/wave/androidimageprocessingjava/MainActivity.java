9c642cea189dc78d332acfb22d528fef504b157e
Rafał Połaski <rafal.polaski@gmail.com>
2016-03-22 17:59:19 +0100

    Add menu fragment

--- app/src/main/java/com/example/wave/androidimageprocessingjava/MainActivity.java
+++ app/src/main/java/com/example/wave/androidimageprocessingjava/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.wave.androidimageprocessingjava;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +13,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public final int PickImageId = 666;
+    private ImageView editedImageView;
+    public static android.net.Uri editedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+
+        MenuFragment menuFragment = new MenuFragment(this);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.menuContainer, menuFragment);
+        fragmentTransaction.commit();
+
+        this.editedImageView = (ImageView) findViewById(R.id.editedImageView);
+        this.editedImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PickImageId);
             }
         });
     }
@@ -49,4 +70,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PickImageId && resultCode == RESULT_OK && data != null){
+            editedImageUri = data.getData();
+            editedImageView.setImageURI(editedImageUri);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/wave/androidimageprocessingjava/MenuFragment.java
@@ -0,0 +1,58 @@
+package com.example.wave.androidimageprocessingjava;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MenuFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuFragment extends Fragment {
+
+    Context context;
+
+    public MenuFragment(Context context) {
+        super();
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        ImageButton goToEdit = (ImageButton) view.findViewById(R.id.goToEdit);
+        goToEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(MainActivity.editedImageUri != null){
+
+                }else{
+                    Toast.makeText(context, "First choose image", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
+    }
+
+
+}
