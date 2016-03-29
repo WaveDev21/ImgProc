ff2867aabc36dc7b94d65cf3b9cb36c26ae8bc39
Rafał Połaski <rafal.polaski@gmail.com>
2016-03-29 20:50:35 +0200

    Edit Activity Repaired

--- app/src/main/java/com/example/wave/androidimageprocessingjava/Edit/RightDrawerFragment.java
+++ app/src/main/java/com/example/wave/androidimageprocessingjava/Edit/RightDrawerFragment.java
@@ -1,18 +1,17 @@
 package com.example.wave.androidimageprocessingjava.Edit;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.renderscript.ScriptC;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.RadioButton;
-import android.widget.Toast;
-
-import com.example.wave.androidimageprocessingjava.MainActivity;
 import com.example.wave.androidimageprocessingjava.R;
+import com.example.wave.androidimageprocessingjava.ScriptC_saturation;
+
+import android.support.v8.renderscript.*;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,10 +25,19 @@ public class RightDrawerFragment extends Fragment {
 
     private Context context;
 
-    public RightDrawerFragment(Context context){
-        super();
-        this.context = context;
+    private ScriptC_saturation mScript;
+
+    // TODO: Rename and change types and number of parameters
+    public static RightDrawerFragment newInstance(Context context) {
+        RightDrawerFragment fragment = new RightDrawerFragment();
+        Bundle args = new Bundle();
+
+        fragment.context = context;
+
+        fragment.setArguments(args);
+        return fragment;
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,8 +47,7 @@ public class RightDrawerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_right_drawer, container, false);
 
         RadioButton button = (RadioButton) view.findViewById(R.id.highLight);
         button.setOnClickListener(new View.OnClickListener() {
