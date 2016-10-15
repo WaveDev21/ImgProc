862525d97e809da7d2935b1db2e3949a178dc596
Rafał Połaski <rafal.polaski@gmail.com>
2016-10-15 13:04:50 +0200

    Zakończono edycje głównego layoutu

--- app/src/main/java/com/example/wave/androidimageprocessingjava/MainActivity.java
+++ app/src/main/java/com/example/wave/androidimageprocessingjava/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.wave.androidimageprocessingjava;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
