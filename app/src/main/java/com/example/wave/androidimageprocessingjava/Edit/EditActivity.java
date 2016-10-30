ba85fbfc8d19bb37658ed51ff951c3a4aa8df271
Rafał Połaski <rafal.polaski@gmail.com>
2016-10-30 09:49:44 +0100

    KOmit w trakcie zmainy sliderów

--- app/src/main/java/com/example/wave/androidimageprocessingjava/Edit/EditActivity.java
+++ app/src/main/java/com/example/wave/androidimageprocessingjava/Edit/EditActivity.java
@@ -5,10 +5,12 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -48,6 +50,7 @@ public class EditActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.originalImageView);
 
+        assert imageView != null;
         imageView.setImageURI(MainActivity.editedImageUri);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
@@ -58,77 +61,38 @@ public class EditActivity extends AppCompatActivity {
         mRightDrawer.setTag(1);
 
         mDrawerLayout.setScrimColor(Color.TRANSPARENT);
-        //mLeftDrawer.setBackgroundColor(Color.TRANSPARENT);
-        //mRightDrawer.setBackgroundColor(Color.TRANSPARENT);
+        mDrawerLayout.openDrawer(mRightDrawer);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN, mRightDrawer);
 
         RightDrawerFragment rightDrawer = RightDrawerFragment.newInstance(this, imageView, mLeftDrawer);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.rightDrawer, rightDrawer);
         fragmentTransaction.commit();
-
-        mDrawerListener = new ActionBarDrawerToggle(
-                this,
-                mDrawerLayout,
-                R.string.openDrawer,
-                R.string.closeDrawer
-        ){
-            @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-
-                if (mDrawerLayout.isDrawerOpen(mLeftDrawer)){
-                    mDrawerLayout.closeDrawer(mLeftDrawer);
-                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
-
-                }else{
-                    mDrawerLayout.openDrawer(mLeftDrawer);
-
-                }
-
-                return super.onOptionsItemSelected(item);
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED);
-                super.onDrawerClosed(drawerView);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
-                super.onDrawerOpened(drawerView);
-            }
-        };
-
-        mDrawerLayout.addDrawerListener(mDrawerListener);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        mDrawerListener.syncState();
+//        mDrawerListener.syncState();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-       if(mDrawerListener.onOptionsItemSelected(item))
-           return true;
+//       if(mDrawerListener.onOptionsItemSelected(item))
+//           return true;
 
        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState){
-        if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)){
-            outState.putString("DrawerState", "Opened");
-        }else{
-            outState.putString("DrawerState", "Closed");
-        }
+//        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+//            outState.putString("DrawerState", "Opened");
+//        }else{
+//            outState.putString("DrawerState", "Closed");
+//        }
         super.onSaveInstanceState(outState);
     }
 
@@ -141,6 +105,6 @@ public class EditActivity extends AppCompatActivity {
     @Override
     public void onConfigurationChanged(Configuration newConfig){
         super.onConfigurationChanged(newConfig);
-        mDrawerListener.onConfigurationChanged(newConfig);
+//        mDrawerListener.onConfigurationChanged(newConfig);
     }
 }
