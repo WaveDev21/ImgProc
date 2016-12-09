37ac11b637681b50f436ae4b1024312ba3003531
Rafał Połaski <rafal.polaski@gmail.com>
2016-12-09 09:17:15 +0100

    Naprawiono colorAccent teraz działa jak trzeba

--- app/src/main/rs/colorAccent.rs
+++ app/src/main/rs/colorAccent.rs
@@ -9,54 +9,87 @@ float startBlue;
 
 uchar4 RS_KERNEL colorAccent(uchar4 in)
 {
-    float hsrc, ssrc, vsrc, hdst, sdst, vdst, hfrom, hto;
+    float hSource, sSource, vSource, hDest, sDest, vDest, hFrom, hTo;
     float x, f, i;
 
     uchar4 out = in;
-    float red, grn, blu, val, hue, sat;
+    float red, green, blue, val, hue, sat;
 
     red = startRed/255.0;
-    grn = startGreen/255.0;
-    blu = startBlue/255.0;
+    green = startGreen/255.0;
+    blue = startBlue/255.0;
 
-    x = fmin(fmin(red, grn), blu);
-    vsrc = fmax(fmax(red, grn), blu);
-    if(x == val){
-        hsrc = 0;
-        ssrc = 0;
+    x = fmin(fmin(red, green), blue);
+    vSource = fmax(fmax(red, green), blue);
+    if(x == vSource){
+        hSource = 0;
+        sSource = 0;
     }else{
-        f = (red == x) ? grn - blu : ((grn == x) ? blu - red : red - grn);
-        i = (red == x) ? 3 : ((grn == x) ? 5 : 1);
-        hsrc = fmod((i - f/(val - x)) * 60, 360);
-        ssrc = ((vsrc - x)/vsrc);
+
+        if(red == x){
+            f = green - blue;
+        }else{
+            if(green == x){
+                f = blue - red;
+            }else{
+                f = red - green;
+            }
+        }
+
+        if(red == x){
+            i = 3;
+        }else{
+            if(green == x){
+                i = 5;
+            }else{
+                i = 1;
+            }
+        }
+        hSource = fmod((i-f/(vSource-x)) * 60, 360);
     }
 
 
-    hfrom = fmod((hsrc - (range / 2)) + 360, 360);
-    hto = fmod(hsrc + (range / 2), 360);
+    hFrom = fmod(hSource - (range / 2) + 360, 360);
+    hTo = fmod(hSource + (range / 2) + 360, 360);
 
     red = in.r/255.0;
-    grn = in.g/255.0;
-    blu = in.b/255.0;
+    green = in.g/255.0;
+    blue = in.b/255.0;
 
-    x = fmin(fmin(red, grn), blu);
-    vdst = fmax(fmax(red, grn), blu);
-    if(x == val){
-        hdst = 0;
-        sdst = 0;
+    x = fmin(fmin(red, green), blue);
+    vDest = fmax(fmax(red, green), blue);
+    if(x == vDest){
+        hDest = 0;
     }else{
-        f = (red == x) ? grn - blu : ((grn == x) ? blu - red : red - grn);
-        i = (red == x) ? 3 : ((grn == x) ? 5 : 1);
-        hdst = fmod((i - f/(vdst - x)) * 60, 360);
-        sdst = ((vdst - x)/vdst);
+
+        if(red == x){
+            f = green - blue;
+        }else{
+            if(green == x){
+                f = blue - red;
+            }else{
+                f = red - green;
+            }
+        }
+
+        if(red == x){
+            i = 3;
+        }else{
+            if(green == x){
+                i = 5;
+            }else{
+                i = 1;
+            }
+        }
+        hDest = fmod((i-f/(vDest-x)) * 60, 360);
     }
 
-    if (( hfrom <= hto &&
-       hfrom <= hdst &&
-       hto >= hdst) ||
-       ( hfrom > hto &&
-       ( hfrom <= hdst ||
-       hto >= hdst ))) {
+    if (( hFrom <= hTo &&
+       hFrom <= hDest &&
+       hTo >= hDest) ||
+       ( hFrom > hTo &&
+       ( hFrom <= hDest ||
+       hTo >= hDest ))) {
 
     }else{
         out.r = (int) (0.299 * in.r + 0.587 * in.g + 0.114 * in.b);
